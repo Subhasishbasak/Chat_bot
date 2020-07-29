78117fc6aa4e8b4a1c8242f57072d12d62d3fdd4
subhasish <subhasish@cmi.ac.in>
2020-07-29 18:18:20 +0530

    infinite loop + 1st msg bug fixed

--- whatsapp_bot/code/core/clone_chat_bot.py
+++ whatsapp_bot/code/core/clone_chat_bot.py
@@ -26,46 +26,54 @@ def activate_clone_bot(driver):
 
     activation_time = get_time_tuple(str(datetime.datetime.now().time()))
 
-    user = input('\nEnter user name : \n')
+    user = input('\nEnter user name (your name as in the bot account) : \n')
     print('Bot activated for {} at {} hr & {} min'.format(user, activation_time[0], activation_time[1]))
     
     search_chatter(driver, user)
+    otp = 0
 
-    t = (int(input('Enter duration (in minutes) : '))*60)//15
-    
-    for i in range(1,t+2):
-
-        time.sleep(10)
-        time.sleep(int(np.random.uniform(1, 8)))
-    
-        msg = read_last_msg(driver)
+    try:
         
-        m = msg[0].split(' ')
-        last_chat_owner = m[3]+' '+m[4].split(':')[0]
-        last_chat_owner = last_chat_owner.split(':')[0]
+        while True:    
 
-        last_chat_time = get_time_tuple(m[0].split('[')[1])
-        print('\nIteration {}, last chat time {}'.format(i, last_chat_time))
+            time.sleep(int(np.random.uniform(1, 8)))
         
-
-        if (last_chat_time > activation_time) or (last_chat_owner == user):
-            print('\nNew chat detected at {} hr {} min'.format(last_chat_time[0], last_chat_time[1]))
-            last_new_msg = msg[-1]
-            print('\nDetected last new msg : ', last_new_msg)
-
-            reply = str(model.getReply(last_new_msg))
-            #print(reply)
-
-            # Select the Input Box
-            time.sleep(2)
-            input_box = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
-            print('Input box selected')
+            msg = read_last_msg(driver)
             
-            time.sleep(3)
-            input_box.send_keys(reply + Keys.SHIFT + Keys.ENTER)
-            time.sleep(int(np.random.uniform(1,8)))
-            input_box.send_keys(Keys.ENTER)
-            activation_time = last_chat_time
+            m = msg[0].split(' ')
+            last_chat_owner = m[3]+' '+m[4].split(':')[0]
+            last_chat_owner = last_chat_owner.split(':')[0]
+
+            last_chat_time = get_time_tuple(m[0].split('[')[1])
+            print('\nlast chat time {} from {}'.format(last_chat_time, last_chat_owner))
+                
+            #print('act time : {} || last time : {} || otp : {} || condition : {}'.format(activation_time, last_chat_time, otp,  last_chat_time > activation_time))
+            #print('last_chat owner : {} || user : {} || condition1 : {} || condition2 : {} || condition : {}'.format(last_chat_owner, user, last_chat_owner==user, otp>0, (last_chat_owner==user) and (otp>0)))
+
+            if (last_chat_time > activation_time) or ((last_chat_owner==user) and (otp > 0)):
+                otp += 1
+                print('\nNew chat detected at {} hr {} min, from {}'.format(last_chat_time[0], last_chat_time[1], last_chat_owner))
+                last_new_msg = msg[-1]
+                print('\nReplying to last new msg : ', last_new_msg)
+
+                reply = str(model.getReply(last_new_msg))
+                #print(reply)
+
+                # Select the Input Box
+                time.sleep(1)
+                input_box = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
+                print('Input box selected')
+                
+                time.sleep(2)
+                input_box.send_keys(reply + Keys.SHIFT + Keys.ENTER)
+                time.sleep(int(np.random.uniform(1,4)))
+                input_box.send_keys(Keys.ENTER)
+                activation_time = last_chat_time
+
+    except KeyboardInterrupt:
+        print("Exiting")
+        sys.exit()
+
 
 
 
